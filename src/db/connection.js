dfa64a3585ff7500e30fd409b8a27e3fb2731cf6
jaovw <joojvw@gmail.com>
2022-11-08 16:14:50 -0300

    conexao com o atlas

---
+++ src/db/connection.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import { logger } from '../utils/logger.js'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+async function dbConnect() {
+
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@talentacademyapi.9wp5ynr.mongodb.net/?retryWrites=true&w=majority`
+        )
+        logger.info('Conexao bem sucedida!')
+    } catch (e) {
+        logger.error(e)
+    }
+}
+
+export { dbConnect }
