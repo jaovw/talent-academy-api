4a48f3a7533d684c474229f19c9b14c546a4a737
jaovw <joojvw@gmail.com>
2022-11-10 15:54:00 -0300

    ajustando rota de status da aplicacao

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -4,20 +4,21 @@ import { logger } from "../utils/logger.js"
 
 async function status(req, res) {
     try {
-        const con = await dbConnect()
-        const item = await model.findOne().sort({run_t: 1})
+        const conexao = await dbConnect()
+        const item = await model.findOne().sort({ run_t: -1 })
+        const horario = new Date(item.run_t).toLocaleString("pt-BR", { timeZone: 'America/Sao_Paulo' })
 
         return res.status(200).json({
 
             message: 'Aplicacao online ...',
-            info: con,
+            info: conexao,
             memory: {
                 total: process.memoryUsage().heapTotal,
                 usada: process.memoryUsage().heapUsed
             },
             cron: {
-                Job: item.job_name,
-                horario: item.run_t
+                nome: item.job_name,
+                horario: horario
             }
 
         })
