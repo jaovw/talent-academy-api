1ec0b988eab9af39dad653ebdfdd31861855bd7b
jaovw <joojvw@gmail.com>
2022-11-10 14:18:28 -0300

    controller de status da aplicacao

--- src/controllers/statusController.js
+++ src/controllers/statusController.js
@@ -1,15 +1,30 @@
 import { dbConnect } from "../db/connection.js"
+import model from "../models/job.js"
+import { logger } from "../utils/logger.js"
 
 async function status(req, res) {
+    try {
+        const con = await dbConnect()
+        const item = await model.findOne().sort({run_t: 1})
 
-    res.status(200).json({
-        message:'Hello World',
-        info: await dbConnect(),
-        Memoria: {
-            total: process.memoryUsage().heapTotal,
-            usada: process.memoryUsage().heapUsed
-        }
-    })
+        return res.status(200).json({
+
+            message: 'Aplicacao online ...',
+            info: con,
+            memory: {
+                total: process.memoryUsage().heapTotal,
+                usada: process.memoryUsage().heapUsed
+            },
+            cron: {
+                Job: item.job_name,
+                horario: item.run_t
+            }
+
+        })
+    } catch (e) {
+        logger.error(e)
+    }
+
 }
 
 export { status }
