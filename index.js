3000809a84d06c89a15939252e9516daf89f9bd8
jaovw <joojvw@gmail.com>
2022-11-09 21:51:49 -0300

    add cron no server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express"
 import router from "./src/routes.js"
+import { jobDownload, jobTratamento } from "./schedule.js"
 import { logger } from "./src/utils/logger.js"
+
+
 
 const port = process.env.PORT || 3000
 const app = express()
@@ -8,8 +11,12 @@ const app = express()
 app.use(express.json())
 app.use(router)
 
-const server = app.listen(port, () => {
+const server = app.listen(port, async () => {
     logger.info(`API rodando na porta ${port}.`)
+
+    // CRON
+    await jobDownload()
+    await jobTratamento()
 })
 
 export { app, server }
