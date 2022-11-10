3c63da648280362b82f1e9301562acee885710c8
jaovw <joojvw@gmail.com>
2022-11-10 15:52:46 -0300

    aplicando collection criada para controle de jobs

--- schedule.js
+++ schedule.js
@@ -2,21 +2,80 @@ import { schedule } from "node-cron"
 import { logger } from "./src/utils/logger.js"
 import { downloadDados } from "./src/controllers/downloadDados.js"
 import { transformaDados } from "./src/controllers/tranformaDados.js"
+import model from "./src/models/job.js"
+import { dbConnect } from "./src/db/connection.js"
 
 async function jobDownload() {
 
-    return schedule('00 21 * * *', async () => {
-        await downloadDados()
-        logger.info('job 1 rodou')
+    return schedule('43 15 * * *', async () => {
+
+        const time = new Date().getTime()
+
+        const param = {
+            job_name: 'Download',
+            ran: true,
+            run_t: time
+        }
+
+        try {
+
+            await dbConnect()
+
+            let run = await downloadDados()
+
+            if (run !== true) {
+                await model.create({
+                    job_name: 'Download',
+                    ran: false,
+                    run_t: time
+                })
+                return
+            }
+            await model.create(param)
+
+            logger.info('job 1 rodou')
+
+        } catch (e) {
+            logger.error(e)
+        }
 
     })
+
 }
 
-function jobTratamento() {
+async function jobTratamento() {
 
-    return schedule('00 21 * * *', async () => {
-        await transformaDados()
-        logger.info('job 2 rodou')
+    return schedule('44 15 * * *', async () => {
+
+        const time = new Date().getTime()
+
+        const param = {
+            job_name: 'Tratamento',
+            ran: true,
+            run_t: time
+        }
+
+        try {
+
+            await dbConnect()
+
+            let run = await transformaDados()
+
+            if (run !== true) {
+                await model.create({
+                    job_name: 'Tratamento',
+                    ran: false,
+                    run_t: time
+                })
+                return
+            }
+            await model.create(param)
+
+            logger.info('job 2 rodou')
+
+        } catch (e) {
+            logger.error(e)
+        }
 
     })
 }
