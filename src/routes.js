349cbfe4dd220a90460453069fb096dda229ec79
jaovw <joojvw@gmail.com>
2022-11-10 21:09:53 -0300

    rotas com novo controller

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express"
 import { status } from "./controllers/statusController.js"
 import { getProductsId, getProdutos } from "./controllers/listagemController.js"
+import { deleteProdutos } from "./controllers/exclusaoController.js"
 
 const router = Router()
 
@@ -9,5 +10,7 @@ router.get('/', status)
 router.get('/products', getProdutos)
 
 router.get('/products/:id', getProductsId)
+
+router.delete('/products/:id', deleteProdutos)
 
 export default router
