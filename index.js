ffef0967aec410d8b44e5d592a32940965017ac5
jaovw <joojvw@gmail.com>
2022-11-11 18:00:33 -0300

    implementando a documentacao

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import router from "./src/routes.js"
 import { jobDownload, jobTratamento } from "./schedule.js"
 import { logger } from "./src/utils/logger.js"
+import swaggerDocs from "./src/docs/swagger.json" assert {type: "json"}
+import swaggerUi from "swagger-ui-express"
 
 
 
@@ -10,13 +12,14 @@ const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 
 const server = app.listen(port, async () => {
     logger.info(
     `
 
         API rodando na porta ${port}.
-        Caso queira acessar o ambiente local ou Docker >> http://localhost:3000/
+        Caso queira acessar o ambiente local ou Docker >> http://localhost:3000/api-docs
     `)
 
     // CRON
