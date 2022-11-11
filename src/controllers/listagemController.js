d92ce60e7ae15fc63dc726e60eafec166bce6b23
jaovw <joojvw@gmail.com>
2022-11-11 13:05:06 -0300

    aplicando metodo para paginacao

--- src/controllers/listagemController.js
+++ src/controllers/listagemController.js
@@ -4,21 +4,46 @@ import { logger } from "../utils/logger.js"
 
 async function getProdutos(req, res) {
 
+    const pagina = parseInt(req.query.page)
+    const limite = parseInt(req.query.limit)
+
+    const index = (pagina - 1) * limite
+    const end = pagina * limite
+
     try {
-
         await dbConnect()
+        const item = await model.find()
 
-        const listagem = await model.find()
+        const results = {}
 
-        return res.status(200).send(listagem)
+        if (end < item.length) {
+            results.next = {
+                pagina: pagina + 1,
+                limite: limite
+            }
+        }
 
+        if (index > 0) {
+            results.previus = {
+                pagina: pagina - 1,
+                limite: limite
+            }
+        }
+
+
+        results.results = item.slice(index, end)
+
+        res.status(200).json({
+            resultado: results
+        })
     } catch (e) {
         logger.error(e)
-        res.status(500).json({ error: e })
+        res.status(500).json({ error: e.message })
     }
+
 }
 
-async function getProductsId(req, res) {
+async function getProdutosId(req, res) {
 
     const id = req.params.id
 
@@ -32,8 +57,8 @@ async function getProductsId(req, res) {
 
     } catch (e) {
         logger.error(e)
-        res.status(500).json({ error: e })
+        res.status(500).json({ error: e.message })
     }
 }
 
-export { getProdutos, getProductsId }
+export { getProdutos, getProdutosId }
